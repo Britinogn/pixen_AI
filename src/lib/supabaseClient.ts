0ee09916d5f8e3e0dc4951c12ce0179c1dcfc54c
Tino Nova Tech <124672998+Britinogn@users.noreply.github.com>
2026-02-23 21:42:04 +0100

    ok

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,5 +1,17 @@
 // src/lib/supabaseClient.ts
-import { createClient } from '@supabase/supabase-js'
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from '../utils/constant'
+// import { createClient } from '@supabase/supabase-js'
+// import { SUPABASE_URL, SUPABASE_ANON_KEY } from '../utils/constant'
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+// export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Missing Supabase environment variables.')
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
